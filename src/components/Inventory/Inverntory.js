e61a3bacc88e303d4ddcbf7981fcf6ae74d5f351
saruarnazmul <saruarhossennazmul0@gmail.com>
2021-11-22 01:42:00 +0600

    added inventroy

---
+++ src/components/Inventory/Inverntory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inverntory = () => {
+    return (
+        <div>
+            <h2>this is enventory</h2>
+        </div>
+    );
+};
+
+export default Inverntory;
